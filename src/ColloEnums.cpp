7c6d8b12101cf26b76f08763e077497ec2f87686
Arnaud-Gagner <agagner.work@gmail.com>
2025-04-01 15:48:51 -0400

    Avoiding involuntary fallthrough

--- src/ColloEnums.cpp
+++ src/ColloEnums.cpp
@@ -5,20 +5,26 @@ std::string levelToString(const LogLevel& lvl)
     std::string level;
     switch (lvl)
     {
-        case LogLevel::debug:
-            level = "DEBUG";
-            break;
-        case LogLevel::info:
-            level = "INFO";
-            break;
-        case LogLevel::fatal:
-            level = "FATAL";
-            break;
-        case LogLevel::warn:
-            level = "WARN";
-        default:
-            level = "INVALID_LEVEL";
-            break;
+    case LogLevel::debug: {
+        level = "DEBUG";
+        break;
+    }
+    case LogLevel::info: {
+        level = "INFO";
+        break;
+    }
+    case LogLevel::fatal: {
+        level = "FATAL";
+        break;
+    }
+    case LogLevel::warn: {
+        level = "WARN";
+        break;
+    }
+    default: {
+        level = "INVALID_LEVEL";
+        break;
+    }
     }
     return level;
 }
